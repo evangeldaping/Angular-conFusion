9fb342b969b80865d7d0dcd0aac95763bdec841e
evangel <evangeldaping@gmail.com>
2020-11-14 12:43:10 -0800

    Template Driven-Forms Part 1

--- conFusion/src/app/login/login.component.ts
+++ conFusion/src/app/login/login.component.ts
@@ -8,9 +8,16 @@ import {MatDialog, MatDialogRef} from '@angular/material';
 })
 export class LoginComponent implements OnInit {
 
+  user = {username: '', password: '', remember: false};
+
   constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('User: ', this.user);
+    this.dialogRef.close();
   }
 
 }
