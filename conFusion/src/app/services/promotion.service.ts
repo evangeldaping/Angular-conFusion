5224151bf47699bfb4ebe95a42a13cea78bffdef
evangel <evangeldaping@gmail.com>
2020-11-18 14:16:38 -0800

    Promise Part 2

--- conFusion/src/app/services/promotion.service.ts
+++ conFusion/src/app/services/promotion.service.ts
@@ -7,13 +7,23 @@ export class PromotionService {
 
   constructor() { }
   getPromotions(): Promise<Promotion[]>{
-    return Promise.resolve(PROMOTIONS);
-  }
-  getPromotion(id: number): Promise<Promotion>{
-    return Promise.resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]);
-  }
-  getFeaturedPromotion(): Promise<Promotion>{
-    return Promise.resolve(PROMOTIONS.filter((promo) => (promo.featured))[0]);
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second dela
+      setTimeout(() => resolve(PROMOTIONS), 2000);
+    });
   }
 
+  getPromotion(id: string): Promise<Promotion>{
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
+    });
+  }
+
+  getFeaturedPromotion(): Promise<Promotion>{
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.featured))[0]), 2000);
+    });
+  }
 }
