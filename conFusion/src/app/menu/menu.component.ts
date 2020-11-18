5bf10dd0ffa0dcd16479a9037e1b692a6780e92c
evangel <evangeldaping@gmail.com>
2020-11-18 14:29:18 -0800

    RxJS Part 1

--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
-
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -14,7 +13,7 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() {
     this.dishService.getDishes()
-    .then(dishes=> this.dishes = dishes );
+    .subscribe(dishes=> this.dishes = dishes );
   }
   onSelect(dish: Dish){
     this.selectedDish= dish;
